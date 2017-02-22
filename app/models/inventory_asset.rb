254306e6af14fbefbbe0a5fbd68676f74ccbdadc
ljcheng <ljcheng@thoughtworks.com>
2017-02-22 14:36:01 +0800

    add association inventory asset belongs to description

--- app/models/inventory_asset.rb
+++ app/models/inventory_asset.rb
@@ -1,2 +1,6 @@
 class InventoryAsset < ApplicationRecord
+  belongs_to :description,
+             ->(asset){ where(instanceid: asset.instanceid) },
+             class_name: 'InventoryDescription', primary_key: :classid, foreign_key: :classid
+
 end
