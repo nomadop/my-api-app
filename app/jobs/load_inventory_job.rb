b78e49cc5b06ba92c35bc5b7addd92a92926e2dc
ljcheng <ljcheng@thoughtworks.com>
2017-02-22 13:50:03 +0800

    finish basic load inventory job

--- app/jobs/load_inventory_job.rb
+++ app/jobs/load_inventory_job.rb
@@ -2,7 +2,47 @@ class LoadInventoryJob < ApplicationJob
   queue_as :default
 
   def perform()
-    puts 'Loading Inventory...'
-    sleep 60
+    steam_id = Authentication.steam_id
+    cookie = Authentication.cookie
+    return if steam_id.empty? or cookie.empty?
+
+    option = {
+        method: :get,
+        url: "http://steamcommunity.com/inventory/#{steam_id}/753/6?l=schinese'",
+        headers: {
+            :Accept => '*/*',
+            :'Accept-Encoding' => 'gzip, deflate, sdch',
+            :'Accept-Language' => 'zh-CN,zh;q=0.8,en;q=0.6,ja;q=0.4,zh-TW;q=0.2',
+            :'Cache-Control' => 'no-cache',
+            :'Connection' => 'keep-alive',
+            :'Cookie' => cookie,
+            :'Host' => 'steamcommunity.com',
+            :'Pragma' => 'no-cache',
+            :'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+        },
+        proxy: 'http://127.0.0.1:8888'
+    }
+    response = RestClient::Request.execute(option)
+
+    if response.code == 200
+      result = JSON.parse(response.body)
+      assets = result['assets']
+      InventoryAsset.transaction do
+        assets.each do |json|
+          asset = InventoryAsset.find_or_create_by(assetid: json['assetid'])
+          asset.update(json)
+        end
+      end
+      descriptions = result['descriptions']
+      InventoryDescription.transaction do
+        descriptions.each do |json|
+          description = InventoryDescription.find_or_create_by(
+              classid: json['classid'],
+              instanceid: json['instanceid']
+          )
+          description.update(json)
+        end
+      end
+    end
   end
 end
