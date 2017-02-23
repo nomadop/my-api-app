477853c71a3d6f69296c22367b0b1b99037a3034
ljcheng <ljcheng@thoughtworks.com>
2017-02-23 15:59:03 +0800

    add refresh method in authentication

--- lib/authentication.rb
+++ lib/authentication.rb
@@ -35,6 +35,35 @@ class Authentication
       self.account = params[:account] if params[:account]
       self.cookie = params[:cookie] if params[:cookie]
     end
+
+    def refresh
+      uri = URI('http://store.steampowered.com')
+      parse_cookie = Proc.new {|c| HTTP::Cookie.parse(c, uri)}
+      jar = cookie.split(';').flat_map(&parse_cookie).reduce(HTTP::CookieJar.new, &:add)
+
+      option = {
+          method: :get,
+          url: 'https://store.steampowered.com/login/checkstoredlogin/?redirectURL=0',
+          headers: {
+              :Accept => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+              :'Accept-Encoding' => 'gzip, deflate, sdch, br',
+              :'Accept-Language' => 'zh-CN,zh;q=0.8,en;q=0.6,ja;q=0.4,zh-TW;q=0.2',
+              :'Cache-Control' => 'no-cache',
+              :'Connection' => 'keep-alive',
+              :'Content-Type' => 'application/x-www-form-urlencoded; charset=UTF-8',
+              :'Cookie' => cookie,
+              :'Host' => 'store.steampowered.com',
+              :'Pragma' => 'no-cache',
+              :'Upgrade-Insecure-Requests' => 1,
+              :'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+          },
+          proxy: 'http://127.0.0.1:8888',
+          ssl_ca_file: 'config/certs/ca_certificate.pem',
+      }
+      response = RestClient::Request.execute(option)
+      response.cookie_jar.cookies.reduce(jar, &:add)
+      self.cookie = jar.cookies.join(';')
+    end
   end
 
   @redis = Redis.new(db: 15)
