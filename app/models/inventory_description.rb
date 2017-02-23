7dbdb7db9a3b67b238ff2b96d9b665d81a59e444
ljcheng <ljcheng@thoughtworks.com>
2017-02-23 15:12:26 +0800

    add association of inventory description has many assets

--- app/models/inventory_description.rb
+++ app/models/inventory_description.rb
@@ -2,4 +2,7 @@ class InventoryDescription < ApplicationRecord
   include ActAsGooItem
   self.inheritance_column = nil
 
+  has_many :assets,
+           ->(description){ where(instanceid: description.instanceid) },
+           class_name: 'InventoryAsset', primary_key: :classid, foreign_key: :classid
 end
