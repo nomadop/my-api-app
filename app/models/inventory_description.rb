ecefd9d385e65e7100f09c26225156eee812d768
ljcheng <ljcheng@thoughtworks.com>
2017-02-24 11:33:56 +0800

    add association between market asset and inventory description

--- app/models/inventory_description.rb
+++ app/models/inventory_description.rb
@@ -5,4 +5,5 @@ class InventoryDescription < ApplicationRecord
   has_many :assets,
            ->(description){ where(instanceid: description.instanceid) },
            class_name: 'InventoryAsset', primary_key: :classid, foreign_key: :classid
+  belongs_to :market_asset, foreign_key: :classid
 end
--- app/models/market_asset.rb
+++ app/models/market_asset.rb
@@ -3,6 +3,7 @@ class MarketAsset < ApplicationRecord
   self.inheritance_column = nil
   self.primary_key = :classid
 
+  has_one :inventory_description, foreign_key: :classid
   has_one :order_histogram, primary_key: :item_nameid, foreign_key: :item_nameid
 
   def load_order_histogram
