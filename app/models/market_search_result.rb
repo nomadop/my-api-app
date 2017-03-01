14a547be303bdafc0ab8a80cc1f3802c9cfcec03
ljcheng <ljcheng@thoughtworks.com>
2017-03-01 16:32:57 +0800

    add load market asset method for market search result

--- app/models/market_search_result.rb
+++ app/models/market_search_result.rb
@@ -1,2 +1,12 @@
 class MarketSearchResult < ApplicationRecord
+  has_one :market_asset
+
+  def load_market_asset
+    return unless market_asset.nil?
+
+    MarketAsset.transaction do
+      self.market_asset = Market.load_asset_by_url(listing_url)
+      save
+    end
+  end
 end
