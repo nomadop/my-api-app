0d032540973e8d83fe544ccf18b45dff43f4b124
ljcheng <ljcheng@thoughtworks.com>
2017-03-03 10:55:55 +0800

    rescue 429 error

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -9,6 +9,12 @@ class ApplicationJob < ActiveJob::Base
     end
   end
 
+  rescue_from(RestClient::TooManyRequests) do
+    '429 Too Many Requests, waiting for 3 minutes...'
+    sleep 3.minutes
+    retry_job
+  end
+
   rescue_from(Exception) do |exception|
     ps = Sidekiq::ProcessSet.new
     ps.each do |process|
