fa54cbb2f84bd991697d480e03b0bc5991cbb92d
ljcheng <ljcheng@thoughtworks.com>
2017-03-14 16:51:32 +0800

    scan market after create steam app

--- app/models/steam_app.rb
+++ app/models/steam_app.rb
@@ -1,13 +1,19 @@
 class SteamApp < ApplicationRecord
   self.inheritance_column = nil
 
+  after_create :scan_market
+
   has_many :market_assets, primary_key: :steam_appid, foreign_key: :market_fee_app
 
   scope :with_trading_cards, -> { where('categories @> ?', [{id: 29}].to_json) }
   scope :with_market_assets, -> { joins(:market_assets).distinct }
   scope :without_market_assets, -> { left_outer_joins(:market_assets).where(market_assets: {market_fee_app: nil}) }
 
+  def with_trading_cards?
+    categories.any? { |c| c['id'] == 29}
+  end
+
   def scan_market
-    Market.scan(steam_appid)
+    Market.scan(steam_appid) if with_trading_cards?
   end
 end
