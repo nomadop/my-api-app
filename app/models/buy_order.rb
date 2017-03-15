81388cf6d5be5c3be93b700542365024da7fa52b
ljcheng <ljcheng@thoughtworks.com>
2017-03-15 20:39:34 +0800

    add association between order histogram and buy order

--- app/models/buy_order.rb
+++ app/models/buy_order.rb
@@ -4,11 +4,15 @@ class BuyOrder < ApplicationRecord
   after_create :refresh_status_later
 
   belongs_to :market_asset, primary_key: :market_hash_name, foreign_key: :market_hash_name
+  has_one :order_histogram, through: :market_asset
 
   scope :success, -> { where(success: 1) }
   scope :active, -> { where(active: 1) }
   scope :cancelable, ->(ppg = 0.5) { joins(:market_asset).where('active = 1 AND 1.0 * price / market_assets.goo_value > ?', ppg) }
   default_scope { where(success: 1) }
+
+  delegate :load_order_histogram, to: :market_asset
+  delegate :lowest_sell_order, to: :order_histogram
 
   def refresh_status
     status = Market.get_buy_order_status(buy_orderid)
