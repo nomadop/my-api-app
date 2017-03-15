ce9d3ecf457fd7f71226372709078d796aac3ee7
ljcheng <ljcheng@thoughtworks.com>
2017-03-15 09:58:58 +0800

    update scope of steam app

--- app/models/steam_app.rb
+++ app/models/steam_app.rb
@@ -6,11 +6,12 @@ class SteamApp < ApplicationRecord
   has_many :market_assets, primary_key: :steam_appid, foreign_key: :market_fee_app
 
   scope :with_trading_cards, -> { where('categories @> ?', [{id: 29}].to_json) }
+  scope :without_trading_cards, -> { where.not('categories @> ?', [{id: 29}].to_json) }
   scope :with_market_assets, -> { joins(:market_assets).distinct }
   scope :without_market_assets, -> { left_outer_joins(:market_assets).where(market_assets: {market_fee_app: nil}) }
 
   def with_trading_cards?
-    categories.any? { |c| c['id'] == 29}
+    categories&.any? { |c| c['id'] == 29}
   end
 
   def scan_market
