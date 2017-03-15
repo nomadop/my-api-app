20b6cdd1bf5cefed00d190ebb403021c42268345
ljcheng <ljcheng@thoughtworks.com>
2017-03-15 18:34:17 +0800

    fix buy order model

--- app/models/buy_order.rb
+++ app/models/buy_order.rb
@@ -1,12 +1,14 @@
 class BuyOrder < ApplicationRecord
-  Include ActAsListable
+  include ActAsListable
 
   after_create :refresh_status_later
 
   belongs_to :market_asset, primary_key: :market_hash_name, foreign_key: :market_hash_name
 
+  scope :success, -> { where(success: 1) }
   scope :active, -> { where(active: 1) }
   scope :cancelable, ->(ppg = 0.5) { joins(:market_asset).where('active = 1 AND 1.0 * price / market_assets.goo_value > ?', ppg) }
+  default_scope { where(success: 1) }
 
   def refresh_status
     status = Market.get_buy_order_status(buy_orderid)
