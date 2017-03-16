436d37314f5b58b71ea8b5400f27e800dfba94cf
ljcheng <ljcheng@thoughtworks.com>
2017-03-16 15:57:18 +0800

    fix date time setter of sell history

--- app/models/sell_history.rb
+++ app/models/sell_history.rb
@@ -2,6 +2,7 @@ class SellHistory < ApplicationRecord
   belongs_to :market_asset, primary_key: :classid, foreign_key: :classid
 
   def datetime=(datetime)
-    datetime.is_a?(DateTime) ? super : super(DateTime.parse(datetime))
+    datetime = DateTime.strptime(datetime, '%b %d %Y %H: %z').to_time if datetime.is_a?(String)
+    super(datetime)
   end
 end
