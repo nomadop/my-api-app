86061801eb6f20bbcb010b4413c4aced00a76662
ljcheng <ljcheng@thoughtworks.com>
2017-03-16 14:00:21 +0800

    override datetime setter of sell history

--- app/models/sell_history.rb
+++ app/models/sell_history.rb
@@ -1,3 +1,7 @@
 class SellHistory < ApplicationRecord
   belongs_to :market_asset, primary_key: :classid, foreign_key: :classid
+
+  def datetime=(datetime)
+    datetime.is_a?(DateTime) ? super : super(DateTime.parse(datetime))
+  end
 end
