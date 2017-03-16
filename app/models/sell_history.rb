94160ebd6c0947ee1aaca3c9a7b2892cafef5c8c
ljcheng <ljcheng@thoughtworks.com>
2017-03-16 16:54:09 +0800

    add scopes to sell history

--- app/models/sell_history.rb
+++ app/models/sell_history.rb
@@ -1,6 +1,27 @@
 class SellHistory < ApplicationRecord
   belongs_to :market_asset, primary_key: :classid, foreign_key: :classid
 
+  scope :with_in, ->(duration) { where('datetime > ?', duration.ago) }
+  scope :higher_than, ->(price) { where('price > ?', price) }
+
+  class << self
+    def total_amount
+      sum(:amount)
+    end
+
+    def total_price
+      sum('price * amount')
+    end
+
+    def avg_price
+      pluck('SUM(price * amount) / SUM(amount)').first
+    end
+
+    def sell_rate(price)
+      1.0 * higher_than(price).total_amount / total_amount
+    end
+  end
+
   def datetime=(datetime)
     datetime = DateTime.strptime(datetime, '%b %d %Y %H: %z').to_time if datetime.is_a?(String)
     super(datetime)
