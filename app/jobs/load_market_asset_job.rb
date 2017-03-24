6c4e96b2bf2a5e6ab08ed5325701df71b719f92e
ljcheng <ljcheng@thoughtworks.com>
2017-03-24 16:03:31 +0800

    set timeout of load market asset job to 12 second

--- app/jobs/load_market_asset_job.rb
+++ app/jobs/load_market_asset_job.rb
@@ -2,7 +2,9 @@ class LoadMarketAssetJob < ApplicationJob
   queue_as :market_asset
 
   def perform(url, market_hash_name = nil)
-    Market.load_asset_by_url(url) if url
-    Market.load_asset_by_hash_name(market_hash_name) if market_hash_name
+    Utility.timeout(12) do
+      Market.load_asset_by_url(url) if url
+      Market.load_asset_by_hash_name(market_hash_name) if market_hash_name
+    end
   end
 end
