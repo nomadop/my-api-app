eeacdbbb0d5b325e3282851e9451c58512103d87
ljcheng <ljcheng@thoughtworks.com>
2017-03-27 00:08:33 +0800

    add market asset type method to my listing

--- app/models/my_listing.rb
+++ app/models/my_listing.rb
@@ -5,6 +5,7 @@ class MyListing < ApplicationRecord
 
   has_one :market_asset, primary_key: :market_hash_name, foreign_key: :market_hash_name
   has_one :order_histogram, through: :market_asset
+  has_one :steam_app, through: :market_asset
 
   scope :cancelable, -> { joins(:order_histogram).where('price > order_histograms.lowest_sell_order OR (price > 100 AND price = order_histograms.lowest_sell_order AND CAST(order_histograms.sell_order_graph->0->>1 AS int) - (SELECT COUNT(*) FROM "my_listings" INNER JOIN "market_assets" ON "my_listings"."market_hash_name" = "market_assets"."market_hash_name" WHERE "market_assets"."item_nameid" = order_histograms.item_nameid) > 0)') }
 
@@ -52,6 +53,10 @@ class MyListing < ApplicationRecord
 
   def cancel
     Market.cancel_my_listing(listingid)
+  end
+
+  def market_asset_type
+    market_asset.type
   end
 
   def load_market_asset_later
