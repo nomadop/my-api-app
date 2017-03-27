f63559818a1e85a7ea2cd3046ae3f2529bace7cb
ljcheng <ljcheng@thoughtworks.com>
2017-03-27 11:35:39 +0800

    update marketable date method of inventory description

--- app/models/inventory_description.rb
+++ app/models/inventory_description.rb
@@ -18,7 +18,7 @@ class InventoryDescription < ApplicationRecord
   def marketable_date
     matches = owner_descriptions&.map do |description|
       match = description['value'].match(/\[date\](?<timestamp>\d+)\[\/date\]后可交易、可出售/)
-      match && Time.at(match[:timestamp].to_i).to_date
+      match && Time.at(match[:timestamp].to_i).to_date.to_s
     end
     matches && matches.compact.first
   end
