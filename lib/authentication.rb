657fbd61eed85664d33e1d5311b529f7f2f78a29
ljcheng <ljcheng@thoughtworks.com>
2017-06-08 15:13:06 +0800

    add login method to authentication

--- lib/authentication.rb
+++ lib/authentication.rb
@@ -1,3 +1,5 @@
+require 'execjs'
+
 class Authentication
   class << self
     attr_reader :default_account
@@ -27,6 +29,71 @@ class Authentication
           ssl_ca_file: 'config/certs/ca_certificate.pem',
       }
       RestClient::Request.execute(option)
+    end
+
+    def get_rsa_key(username)
+      option = {
+          method: :post,
+          url: 'https://store.steampowered.com/login/getrsakey',
+          headers: {
+              :'Content-Type' => 'application/x-www-form-urlencoded; charset=UTF-8',
+              :'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+          },
+          payload: {
+              username: username,
+          },
+          proxy: 'http://127.0.0.1:8888',
+          ssl_ca_file: 'config/certs/ca_certificate.pem',
+      }
+      response = RestClient::Request.execute(option)
+      JSON.parse(response.body)
+    end
+
+    def do_login(username, password, rsa_timestamp, two_factory_code = '')
+      option = {
+          method: :post,
+          url: 'https://store.steampowered.com/login/dologin',
+          headers: {
+              :'Content-Type' => 'application/x-www-form-urlencoded; charset=UTF-8',
+              :'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+          },
+          payload: {
+              username: username,
+              password: password,
+              emailauth: '',
+              twofactorcode: two_factory_code,
+              loginfriendlyname: '',
+              captchagid: -1,
+              captcha_text: '',
+              emailsteamid: '',
+              remember_login: true,
+              rsatimestamp: rsa_timestamp,
+          },
+          proxy: 'http://127.0.0.1:8888',
+          ssl_ca_file: 'config/certs/ca_certificate.pem',
+      }
+      response = RestClient::Request.execute(option)
+      JSON.parse(response.body)
+    end
+
+    def login(username, password)
+      rsa_key = get_rsa_key(username)
+      password.gsub!(/[^\x00-\x7F]/, '')
+
+      rsa_js_source = File.read('lib/rsa.js')
+      js_context = ExecJS.compile(rsa_js_source)
+      encrypted_password = js_context.eval <<-JAVASCRIPT
+        RSA.encrypt(
+          '#{password}', 
+          RSA.getPublicKey('#{rsa_key['publickey_mod']}', '#{rsa_key['publickey_exp']}')
+        )
+      JAVASCRIPT
+
+      rsa_timestamp = rsa_key['timestamp']
+      do_login(username, encrypted_password, rsa_timestamp)
+      puts 'input two factory code:'
+      two_factory_code = gets.chomp
+      do_login(username, encrypted_password, rsa_timestamp, two_factory_code)
     end
   end
 
