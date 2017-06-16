f442322202508c9f33cf09cccf1188d2ebc9062a
ljcheng <ljcheng@thoughtworks.com>
2017-06-16 11:32:09 +0800

    update pull order activity job

--- app/jobs/pull_order_activity_job.rb
+++ app/jobs/pull_order_activity_job.rb
@@ -2,8 +2,10 @@ class PullOrderActivityJob < ApplicationJob
   queue_as :default
 
   def perform(item_nameid)
-    Market.pull_order_activity(item_nameid)
+    Utility.timeout(5.seconds) do
+      Market.pull_order_activity(item_nameid)
+    end
 
-    ApplicationJob.perform_unique(PullOrderActivityJob, item_nameid, wait: 5.seconds)
+    ApplicationJob.perform_unique(PullOrderActivityJob, item_nameid)
   end
 end
