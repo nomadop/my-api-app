aff0ad8f3c0fa1fd940e30fd32ed510e83651561
ljcheng <ljcheng@thoughtworks.com>
2017-06-16 15:33:20 +0800

    add methods to steam user

--- app/models/steam_user.rb
+++ app/models/steam_user.rb
@@ -1,2 +1,19 @@
 class SteamUser < ApplicationRecord
+  def load_profile_data
+    profile_data = Steam.get_profile_data(profile_url)
+    update(steamid: profile_data['steamid'])
+  end
+
+  def set_nickname(nickname)
+    result = Steam.set_nickname(self, nickname)
+    update(nickname: nickname) if result['success'] == 1
+  end
+
+  def add_friend
+    Steam.add_friend(self)
+  end
+
+  def send_comment(comment)
+    Steam.send_comment(self, comment)
+  end
 end
