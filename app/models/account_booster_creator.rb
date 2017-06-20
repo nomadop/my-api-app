073ef57dc297dfad9159f6282d748c1f38bf5296
ljcheng <ljcheng@thoughtworks.com>
2017-06-20 17:39:29 +0800

    add available? method to account booster creator

--- app/models/account_booster_creator.rb
+++ app/models/account_booster_creator.rb
@@ -1,4 +1,9 @@
 class AccountBoosterCreator < ApplicationRecord
   belongs_to :account
   belongs_to :booster_creator, primary_key: :appid, foreign_key: :appid
+
+  def available?
+    return true if available_at_time.nil?
+    Time.now > DateTime.parse(available_at_time)
+  end
 end
