d4bdc49b8bde3f96d07c3e761a71983e18b607a9
ljcheng <ljcheng@thoughtworks.com>
2017-07-04 13:59:33 +0800

    add available time method to account booster creator

--- app/models/account_booster_creator.rb
+++ app/models/account_booster_creator.rb
@@ -10,6 +10,11 @@ class AccountBoosterCreator < ApplicationRecord
   end
 
   def available_at
-    available_at_time.blank? ? Time.at(0) : DateTime.parse(available_at_time).to_time
+    available_at_time.blank? ? Time.at(0) : available_time
+  end
+
+  def available_time
+    time_str = available_at_time.gsub('下午', 'PM').gsub('上午', 'AM')
+    DateTime.strptime(time_str, '%m月%d日%P%I:%M').to_time
   end
 end
