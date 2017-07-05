f487d6c3d5422759192d7525f4c424da34bd66c3
ljcheng <ljcheng@thoughtworks.com>
2017-07-05 14:53:04 +0800

    add email address to account

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170704080643) do
+ActiveRecord::Schema.define(version: 20170704081847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +29,9 @@ ActiveRecord::Schema.define(version: 20170704080643) do
     t.string   "account_name"
     t.string   "account_id"
     t.text     "cookie"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.string   "email_address"
   end
 
   create_table "booster_creators", force: :cascade do |t|
