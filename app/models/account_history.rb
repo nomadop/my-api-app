72f07ea37bdd07893a9318aa07672bdd4bd19b53
ljcheng <ljcheng@thoughtworks.com>
2017-07-05 15:55:56 +0800

    add scopes to account history

--- app/models/account_history.rb
+++ app/models/account_history.rb
@@ -1,3 +1,10 @@
 class AccountHistory < ApplicationRecord
   self.inheritance_column = nil
+
+  scope :with_in, ->(duration) { where('date > ?', duration.ago) }
+  scope :between, ->(from, to) { where(date: (from..to)) }
+  scope :market, -> { where(items: 'Steam 社区市场') }
+  scope :wallet, -> { where('items like ?', '已购买%钱包资金') }
+  scope :income, -> { where('change > 0') }
+  scope :expense, -> { where('change < 0') }
 end
