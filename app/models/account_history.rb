26cd3a94f0736365a2f85ce43c4916077e923ae3
ljcheng <ljcheng@thoughtworks.com>
2017-07-06 16:54:24 +0800

    update scopes of account history

--- app/models/account_history.rb
+++ app/models/account_history.rb
@@ -3,8 +3,8 @@ class AccountHistory < ApplicationRecord
 
   scope :with_in, ->(duration) { where('date > ?', duration.ago) }
   scope :between, ->(from, to) { where(date: (from..to)) }
-  scope :market, -> { where(items: 'Steam 社区市场') }
-  scope :wallet, -> { where('items like ?', '已购买%钱包资金') }
+  scope :market, -> { where("items->>0 = 'Steam 社区市场'") }
+  scope :wallet, -> { where('items->>0 like ?', '已购买%钱包资金') }
   scope :income, -> { where('change > 0') }
   scope :expense, -> { where('change < 0') }
 end
