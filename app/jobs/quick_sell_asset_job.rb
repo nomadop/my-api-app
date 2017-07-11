b997de87a39009b203b9bd96916079f0bef8cff5
ljcheng <ljcheng@thoughtworks.com>
2017-07-11 10:22:28 +0800

    update quick sell asset job

--- app/jobs/quick_sell_asset_job.rb
+++ app/jobs/quick_sell_asset_job.rb
@@ -1,12 +1,19 @@
 class QuickSellAssetJob < ApplicationJob
   queue_as :quick_sell
 
+  EXIST_MESSAGE = '您已上架该物品并正等待确认。请确认或撤下现有的上架物品。'
+
   def perform(id)
     InventoryAsset.find(id).quick_sell
   end
 
-  rescue_from(RestClient::BadGateway) do
-    puts 'Sell item failed...'
-    false
+  rescue_from(RestClient::BadGateway) do |e|
+    result = JSON.parse(e.http_body)
+    if result['message'] == EXIST_MESSAGE
+      puts EXIST_MESSAGE
+      false
+    else
+      retry_job
+    end
   end
 end
