1746121835525bc38528136c98c9355620c2a436
ljcheng <ljcheng@thoughtworks.com>
2017-07-12 17:52:25 +0800

    update get notification counts job

--- app/jobs/get_notification_counts_job.rb
+++ app/jobs/get_notification_counts_job.rb
@@ -2,7 +2,7 @@ class GetNotificationCountsJob < ApplicationJob
   queue_as :default
 
   def perform(account_id)
-    Utility.timeout(5) do
+    Utility.timeout(10) do
       @account = Account.find(account_id)
       Steam.get_notification_counts(@account)
     end
