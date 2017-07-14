cc126b69796d55ede76536b11bfd48b5f371f20c
ljcheng <ljcheng@thoughtworks.com>
2017-07-14 15:28:08 +0800

    add scopes to account history

--- app/models/account_history.rb
+++ app/models/account_history.rb
@@ -5,6 +5,9 @@ class AccountHistory < ApplicationRecord
   scope :between, ->(from, to) { where(date: (from..to)) }
   scope :market, -> { where("items->>0 = 'Steam 社区市场'") }
   scope :wallet, -> { where('items->>0 like ?', '已购买%钱包资金') }
+  scope :non_wallet, -> { where.not('items->>0 like ?', '已购买%钱包资金') }
   scope :income, -> { where('change > 0') }
   scope :expense, -> { where('change < 0') }
+  scope :gift, -> { where(type: '礼物购买') }
+  scope :purchase, -> { where(type: '购买') }
 end
