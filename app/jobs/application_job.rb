bf11608885a0ecb09b6ed2ed4085141dfcb11e96
ljcheng <ljcheng@thoughtworks.com>
2017-08-02 16:28:25 +0800

    update perform unique method of application job

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -5,6 +5,10 @@ class ApplicationJob < ActiveJob::Base
       in_queue = queue.any? { |job| job.display_class == job_class.name && job.display_args == args }
       return false if in_queue
 
+      ss = Sidekiq::ScheduledSet.new
+      in_ss = ss.any? { |job| job.display_class == job_class.name && job.display_args == args }
+      return false if in_ss
+
       job_class.set(option).perform_later(*args)
     end
   end
