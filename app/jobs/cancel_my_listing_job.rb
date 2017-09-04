64b6df3a425c7491de62cbc586d26f24fa58ba4b
ljcheng <ljcheng@thoughtworks.com>
2017-09-04 10:51:36 +0800

    update cancel my listing job

--- app/jobs/cancel_my_listing_job.rb
+++ app/jobs/cancel_my_listing_job.rb
@@ -1,8 +1,18 @@
 class CancelMyListingJob < ApplicationJob
   queue_as :cancel_my_listing
 
-  def perform(id)
-    my_listing = MyListing.find(id)
-    my_listing.cancel
+  def perform(listing_id)
+    Market.cancel_my_listing(listing_id)
+  end
+
+  rescue_from(RestClient::BadGateway) do |e|
+    headers = e.http_headers
+    cookies = Utility.parse_cookies(headers[:set_cookie])
+    if cookies.any? { |cookie| cookie.value == 'delete' }
+      Account::DEFAULT.refresh
+      retry_job
+    else
+      false
+    end
   end
 end
