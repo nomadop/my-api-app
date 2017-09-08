abacae222663434dbc9d7b7fad4118a67104ac5e
ljcheng <ljcheng@thoughtworks.com>
2017-09-08 10:34:18 +0800

    fix color text vue component

--- app/javascript/components/color_text.vue
+++ app/javascript/components/color_text.vue
@@ -1,14 +1,14 @@
 <template>
- <td id="color-text" :class="classObject">{{content}}</td>
+    <span id="color-text" :class="classObject">{{content}}</span>
 </template>
 
 <script>
- export default {
-   props: ['color_class', 'content', 'condition'],
-   computed: {
-     classObject: function () {
-       return { [this.color_class]: this.condition(this.content) };
-     }
-   }
- }
+  export default {
+    props: ['color_class', 'content', 'condition'],
+    computed: {
+      classObject: function () {
+        return { [this.color_class]: this.condition(this.content) };
+      }
+    }
+  }
 </script>
