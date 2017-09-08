95097d6e47d4178c3dddb7cc73ba70e32eb5a00b
ljcheng <ljcheng@thoughtworks.com>
2017-09-08 10:11:53 +0800

    add not exist message condition to auto sell and grind job

--- app/jobs/auto_sell_and_grind_job.rb
+++ app/jobs/auto_sell_and_grind_job.rb
@@ -2,6 +2,7 @@ class AutoSellAndGrindJob < ApplicationJob
   queue_as :quick_sell
 
   EXIST_MESSAGE = '您已上架该物品并正等待确认。请确认或撤下现有的上架物品。'
+  NOT_EXIST_MESSAGE = '指定的物品不再存在于您的库存，或者不允许在社区市场交易该物品。'
 
   def perform(id)
     @asset = InventoryAsset.find(id)
@@ -10,7 +11,7 @@ class AutoSellAndGrindJob < ApplicationJob
 
   rescue_from(RestClient::BadGateway) do |e|
     result = JSON.parse(e.http_body)
-    if result['message'] == EXIST_MESSAGE
+    if result['message'] == EXIST_MESSAGE || result['message'] == NOT_EXIST_MESSAGE
       puts EXIST_MESSAGE
       false
     else
