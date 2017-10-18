5c42afbd992d71233a2a7a95920dbdf84bf4a591
ljcheng <ljcheng@thoughtworks.com>
2017-10-18 10:46:31 +0800

    update with in scope of my history

--- app/models/my_history.rb
+++ app/models/my_history.rb
@@ -5,5 +5,5 @@ class MyHistory < ApplicationRecord
   scope :buy, -> { where('who_acted_with like ?', '卖家%') }
   scope :sell, -> { where('who_acted_with like ?', '买家%') }
   scope :booster_pack, -> { where('market_hash_name like ?', '%Booster Pack') }
-  scope :with_in, ->(duration) { where("to_date(listed_date, '已购买: FMMM月DD日') > ?", duration.ago) }
+  scope :with_in, ->(duration) { where('created_at > ?', duration.ago) }
 end
