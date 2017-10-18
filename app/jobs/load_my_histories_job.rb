36945af78e32318e19ab7485d0a2f736018207d6
ljcheng <ljcheng@thoughtworks.com>
2017-10-18 10:44:59 +0800

    update load my histories job

--- app/jobs/load_my_histories_job.rb
+++ app/jobs/load_my_histories_job.rb
@@ -4,11 +4,17 @@ class LoadMyHistoriesJob < ApplicationJob
   def perform(start, count)
     result = Market.request_my_history(start, count)
     return false unless result['success']
+    raise Authentication::AccountExpired if result['total_count'].nil?
 
     import_result = Market.handle_my_history_result(result)
     return if import_result.ids.blank?
 
     tail = start + count
     LoadMyHistoriesJob.perform_later(tail, count) if tail < result['total_count']
+  end
+
+  rescue_from(Authentication::AccountExpired) do
+    Authentication.refresh
+    retry_job
   end
 end
