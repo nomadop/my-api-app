df76c96168417706a84aa8d404bb2789ee49905e
ljcheng <ljcheng@thoughtworks.com>
2018-05-15 11:40:35 +0800

    remove timeout of load market asset job

--- app/jobs/load_market_asset_job.rb
+++ app/jobs/load_market_asset_job.rb
@@ -2,9 +2,7 @@ class LoadMarketAssetJob < ApplicationJob
   queue_as :market_asset
 
   def perform(url, market_hash_name = nil)
-    Utility.timeout(12) do
-      Market.load_asset_by_url(url) if url
-      Market.load_asset_by_hash_name(market_hash_name) if market_hash_name
-    end
+    Market.load_asset_by_url(url) if url
+    Market.load_asset_by_hash_name(market_hash_name) if market_hash_name
   end
 end
