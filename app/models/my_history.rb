3031cf090af438e438da68103ec28ec28ed63907
ljcheng <ljcheng@thoughtworks.com>
2018-05-15 11:33:34 +0800

    add scopes to my history

--- app/models/my_history.rb
+++ app/models/my_history.rb
@@ -4,6 +4,9 @@ class MyHistory < ApplicationRecord
 
   scope :buy, -> { where('who_acted_with like ?', '卖家%') }
   scope :sell, -> { where('who_acted_with like ?', '买家%') }
+  scope :on_sell, -> { where(who_acted_with: '物品上架') }
+  scope :off_sell, -> { where(who_acted_with: '物品下架') }
   scope :booster_pack, -> { where('market_hash_name like ?', '%Booster Pack') }
+  scope :foil, -> { where('market_hash_name like ?', '%Foil%') }
   scope :with_in, ->(duration) { where('created_at > ?', duration.ago) }
 end
