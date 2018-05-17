d0cb1018b960b06eee89bd336a54ec442607a305
ljcheng <ljcheng@thoughtworks.com>
2018-05-17 14:56:58 +0800

    refactor booster creators table

--- app/javascript/pages/booster_creators/booster_creators.vue
+++ app/javascript/pages/booster_creators/booster_creators.vue
@@ -10,31 +10,31 @@
             </p>
         </md-table-toolbar>
         <md-table-row slot="md-table-row" slot-scope="{ item }">
-            <md-table-cell md-label="appid">{{item.appid}}</md-table-cell>
-            <md-table-cell md-label="name">{{item.name}}</md-table-cell>
-            <md-table-cell md-label="price">{{item.price}}</md-table-cell>
-            <md-table-cell md-label="foil avg">{{item.open_price.foil_average}}</md-table-cell>
-            <md-table-cell md-label="ppg">
+            <md-table-cell md-label="Appid">{{item.appid}}</md-table-cell>
+            <md-table-cell md-label="Name">{{item.name}}</md-table-cell>
+            <md-table-cell md-label="Price">{{item.price}}</md-table-cell>
+            <md-table-cell md-label="AVG Foil Price">{{item.open_price.foil_average}}</md-table-cell>
+            <md-table-cell md-label="PPG">
                 <color-text color_class="text-primary"
                             :content="item.price_per_goo"
                             :condition="content => content > 0.57"/>
             </md-table-cell>
-            <md-table-cell md-label="open ppg">
+            <md-table-cell md-label="Open PPG">
                 <color-text color_class="text-primary"
                             :content="item.open_price_per_goo"
                             :condition="content => content > base_ppg"/>
             </md-table-cell>
-            <md-table-cell md-label="open cov">
+            <md-table-cell md-label="Open COV">
                 <color-text color_class="text-primary"
                             :content="item.open_price.coefficient_of_variation"
                             :condition="content => content < 0.15"/>
             </md-table-cell>
-            <md-table-cell md-label="open obr">
+            <md-table-cell md-label="Open OBR">
                 <color-text color_class="text-danger"
                             :content="item.open_price.over_baseline_rate"
                             :condition="content => content < 0.5"/>
             </md-table-cell>
-            <md-table-cell md-label="lc/ic">
+            <md-table-cell md-label="L/I Count">
                 <color-text color_class="text-primary"
                             :content="item.listing_booster_pack_count"
                             :condition="content => content === 0 && item.price_per_goo > 0.57"/>
@@ -43,7 +43,7 @@
                             :content="item.inventory_assets_count"
                             :condition="content => content >= 1"/>
             </md-table-cell>
-            <md-table-cell md-label="open lc/ic">
+            <md-table-cell md-label="Open L/I Count">
                 <color-text color_class="text-danger"
                             :content="item.listing_trading_card_count"
                             :condition="content => content >= 5"/>
@@ -52,26 +52,26 @@
                             :content="item.inventory_cards_count"
                             :condition="content => content >= 3"/>
             </md-table-cell>
-            <md-table-cell md-label="order count">{{item.sell_order_count}} / {{item.buy_order_count}}</md-table-cell>
-            <md-table-cell md-label="proportion">
+            <md-table-cell md-label="Order Count">{{item.sell_order_count}} / {{item.buy_order_count}}</md-table-cell>
+            <md-table-cell md-label="Proportion">
                 <color-text color_class="text-danger"
                             :content="item.sell_proportion"
                             :condition="content => content < 0.1"/>
             </md-table-cell>
-            <md-table-cell md-label="open order count">{{item.open_sell_order_count}} / {{item.open_buy_order_count}}
+            <md-table-cell md-label="Open Order Count">{{item.open_sell_order_count}} / {{item.open_buy_order_count}}
             </md-table-cell>
-            <md-table-cell md-label="open proportion">
+            <md-table-cell md-label="Open Proportion">
                 <color-text color_class="text-danger"
                             :content="item.trading_card_prices_proportion"
                             :condition="content => content < 0.1"/>
             </md-table-cell>
-            <md-table-cell md-label="atime">
+            <md-table-cell md-label="Available Time">
                 <color-text color_class="text-primary"
                             :content="item.min_available_time ? new Date(item.min_available_time) : null"
                             :condition="content => content < new Date()"
                             :filter="content => content ? content.toLocaleTimeString() : null"/>
             </md-table-cell>
-            <md-table-cell md-label="actions">
+            <md-table-cell md-label="Actions" class="action-cell">
                 <md-button class="md-dense md-raised md-primary" @click="create_and_sell(item)">sell</md-button>
                 <md-button class="md-dense md-raised md-primary" @click="create_and_unpack(item)">unpack</md-button>
                 <md-button class="md-dense md-raised md-primary" @click="sell_all_assets(item)">sell assets</md-button>
@@ -79,5 +79,11 @@
         </md-table-row>
     </md-table>
 </template>
+
+<style scoped>
+    .action-cell >>> .md-table-cell-container {
+        width: 365px;
+    }
+</style>
 
 <script src="./booster_creators.js"></script>
