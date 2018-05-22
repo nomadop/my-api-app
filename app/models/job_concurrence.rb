56d8cb45c824910325599a888a660ad15cc81a8b
ljcheng <ljcheng@thoughtworks.com>
2018-05-22 12:31:20 +0800

    add class methods to job concurrence model

--- app/models/job_concurrence.rb
+++ app/models/job_concurrence.rb
@@ -1,12 +1,50 @@
 class JobConcurrence < ApplicationRecord
   enum limit_type: [:block, :throw]
 
+  class << self
+    def start(uuid = nil)
+      raise 'no block given' unless block_given?
+
+      concurrence = create(uuid: uuid || SecureRandom.uuid)
+      yield(concurrence.uuid)
+      concurrence.uuid
+    end
+
+    def wait_for(uuid)
+      concurrence = find_by(uuid: uuid)
+      return if concurrence.nil?
+
+      down_time = 0.second
+      sleep_time = 0.5.second
+      loop do
+        sleep sleep_time
+        down_time = concurrence.reload.completed? ? down_time + sleep_time : 0.second
+        break if down_time > 3.second
+      end
+    end
+
+    def with_concurrence(uuid = nil, &block)
+      raise 'no block given' unless block_given?
+
+      return yield if uuid.nil?
+
+      concurrence = find_by(uuid: uuid)
+      concurrence.with_concurrence(&block)
+    end
+  end
+
   def increase
-    update(concurrence: concurrence + 1)
+    reload.update(concurrence: concurrence + 1)
+  rescue ActiveRecord::StaleObjectError
+    sleep 0.1.second
+    increase
   end
 
   def decrease
-    update(concurrence: concurrence - 1)
+    reload.update(concurrence: concurrence - 1)
+  rescue ActiveRecord::StaleObjectError
+    sleep 0.1.second
+    decrease
   end
 
   def limited?
