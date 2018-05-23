4a03d9e767fcfc056bfeaf77cac4c6ebc7394a80
ljcheng <ljcheng@thoughtworks.com>
2018-05-23 17:02:19 +0800

    add asf lib

---
+++ lib/asf.rb
@@ -0,0 +1,16 @@
+class ASF
+  class << self
+    def send_command(command)
+      url = "http://127.0.0.1:1242/Api/Command/#{URI.encode(command)}"
+      option = {
+        method: :post,
+        url: url,
+        headers: {
+          Authentication: ',WrBiVaRd93[7m^E'
+        },
+      }
+      response = RestClient::Request.execute(option)
+      JSON.parse(response.body)
+    end
+  end
+end
