3254704445686bd3ecd42336ac9f59810494736a
ljcheng <ljcheng@thoughtworks.com>
2018-05-31 12:47:27 +0800

    update auto resell job

--- app/jobs/auto_resell_job.rb
+++ app/jobs/auto_resell_job.rb
@@ -2,7 +2,7 @@ class AutoResellJob < ApplicationJob
   queue_as :default
 
   def perform(account_id)
-    MyListing.auto_resell(Account.find(account_id))
+    account_id.nil? ? MyListing.auto_resell_all : MyListing.auto_resell(Account.find(account_id))
     AutoResellJob.set(wait: 30.minutes).perform_later(account_id)
   end
 end
