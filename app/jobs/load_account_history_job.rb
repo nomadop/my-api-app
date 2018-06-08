a240331ff81ec932bea7734c8f16094697465408
ljcheng <ljcheng@thoughtworks.com>
2018-06-08 10:16:24 +0800

    update load account history job

--- app/jobs/load_account_history_job.rb
+++ app/jobs/load_account_history_job.rb
@@ -1,8 +1,8 @@
 class LoadAccountHistoryJob < ApplicationJob
   queue_as :default
 
-  def perform(account_id, cursor = nil)
+  def perform(account_id, cursor = nil, one_page = true)
     next_cursor = Steam.load_account_history(Account.enabled.find(account_id), cursor)
-    LoadAccountHistoryJob.perform_later(account_id, next_cursor) unless next_cursor.nil?
+    LoadAccountHistoryJob.perform_later(account_id, next_cursor) unless next_cursor.nil? || one_page
   end
 end
