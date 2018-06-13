ecd1cae0978759776cfe00f015f83f0a2480441e
ljcheng <ljcheng@thoughtworks.com>
2018-06-13 17:16:19 +0800

    update tor newnym job

--- app/jobs/tor_newnym_job.rb
+++ app/jobs/tor_newnym_job.rb
@@ -2,10 +2,14 @@ class TorNewnymJob < ApplicationJob
   queue_as :tor_newnym
 
   def perform
-    JobConcurrence.transaction do
-      if JobConcurrence.tor.exists?
+    JobConcurrence.tor.not_delegated.find_each do |job_concurrence|
+      instance = TOR.extract_instance(job_concurrence.uuid)
+      if instance.nil?
         Utility.tor_newnym
-        JobConcurrence.tor.destroy_all
+        job_concurrence.destroy
+      else
+        DelegateJob.perform_later('TOR', 'new_nym', instance)
+        job_concurrence.update(delegated: true)
       end
     end
     sleep 3.seconds
