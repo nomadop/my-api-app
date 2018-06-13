97f93146c69bafd73681414361f44f53c2f8052c
ljcheng <ljcheng@thoughtworks.com>
2018-06-13 17:16:07 +0800

    update load market asset job

--- app/jobs/load_market_asset_job.rb
+++ app/jobs/load_market_asset_job.rb
@@ -5,4 +5,8 @@ class LoadMarketAssetJob < ApplicationJob
     Market.load_asset_by_url(url) if url
     Market.load_asset_by_hash_name(market_hash_name) if market_hash_name
   end
+
+  rescue_from(RestClient::TooManyRequests, RestClient::Forbidden, TOR::NoAvailableInstance) do
+    retry_job
+  end
 end
