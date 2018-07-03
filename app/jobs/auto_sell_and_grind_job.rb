ced82ee9d865e87d3732705b56677d22e957bba2
ljcheng <ljcheng@thoughtworks.com>
2018-07-03 10:13:34 +0800

    update auto sell and grind job

--- app/jobs/auto_sell_and_grind_job.rb
+++ app/jobs/auto_sell_and_grind_job.rb
@@ -3,6 +3,7 @@ class AutoSellAndGrindJob < ApplicationJob
 
   EXIST_MESSAGE = '您已上架该物品并正等待确认。请确认或撤下现有的上架物品。'
   NOT_EXIST_MESSAGE = '指定的物品不再存在于您的库存，或者不允许在社区市场交易该物品。'
+  EXPIRED_MESSAGE = '您选择的项目已过期或已不存在。'
   NETWORK_ERROR_MESSAGE = '与网络连接时出现错误。请稍后再试。'
 
   def perform(id)
@@ -10,9 +11,9 @@ class AutoSellAndGrindJob < ApplicationJob
     @asset.auto_sell_and_grind
   end
 
-  rescue_from(RestClient::BadGateway) do |e|
+  rescue_from(RestClient::BadGateway, RestClient::BadRequest) do |e|
     result = JSON.parse(e.http_body)
-    if result['message'] == EXIST_MESSAGE || result['message'] == NOT_EXIST_MESSAGE
+    if [EXIST_MESSAGE, NOT_EXIST_MESSAGE, EXPIRED_MESSAGE].include?(result['message'])
       puts EXIST_MESSAGE
       clean_job_concurrence
     else
