31f408388caf74dfd07b3f84515dc7110028a588
ljcheng <ljcheng@thoughtworks.com>
2018-07-03 10:13:23 +0800

    add timeout for auto resell job

--- app/jobs/auto_resell_job.rb
+++ app/jobs/auto_resell_job.rb
@@ -4,7 +4,7 @@ class AutoResellJob < ApplicationJob
   STEP_NAMES = %w(_ CancelDirty Refresh Reload RefreshPrice CancelCancelable ReloadInventory AutoSell 2FAok)
 
   def perform(step = 1, prev_uuid = nil)
-    job_remains = JobConcurrence.where(uuid: prev_uuid).count
+    job_remains = JobConcurrence.with_in(5.minutes).where(uuid: prev_uuid).count
     if job_remains > 0
       puts "AutoResellJob: Step #{step - 1}(#{STEP_NAMES[step - 1]}) is not finished yet, #{job_remains} jobs left...(#{prev_uuid})"
       return retry_job(wait: 1.second)
