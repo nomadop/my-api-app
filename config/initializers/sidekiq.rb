1ffbc2ca515ea623cb4cd53c096c83947b78d011
ljcheng <ljcheng@thoughtworks.com>
2018-07-19 21:47:14 +0800

    fix deploy

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,11 @@
-host = Rails.configuration.redis['host']
-port = Rails.configuration.redis['port']
-database = Rails.configuration.redis['database']['sidekiq']
-url = "redis://#{host}:#{port}/#{database}"
+redis_config = Rails.configuration.redis
 
-Sidekiq.configure_server { |config| config.redis = { url: url } }
-Sidekiq.configure_client { |config| config.redis = { url: url } }
+if redis_config
+  host = redis_config['host']
+  port = redis_config['port']
+  database = redis_config['database']['sidekiq']
+  url = "redis://#{host}:#{port}/#{database}"
+
+  Sidekiq.configure_server { |config| config.redis = { url: url } }
+  Sidekiq.configure_client { |config| config.redis = { url: url } }
+end
