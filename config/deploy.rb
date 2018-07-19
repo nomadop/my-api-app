b4ae03b1f786f78285b34df548891d6eb03c56bf
ljcheng <ljcheng@thoughtworks.com>
2018-07-19 20:49:31 +0800

    remove tor tmp file from share dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :forward_agent, false     # SSH forward_agent.
 # Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 # Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
 # run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
-set :shared_dirs, fetch(:shared_dirs, []).push('tmp/pids', 'tmp/sockets', 'tmp/tor/')
+set :shared_dirs, fetch(:shared_dirs, []).push('tmp/pids', 'tmp/sockets')
 set :shared_files, fetch(:shared_files, []).push(
   'config/database.yml', 'config/secrets.yml',
   'config/redis.yml', 'config/certs/ca_certificate.pem',
