a746c5dd5cbe58e72a1887e4a3876dfc6c1653e9
ljcheng <ljcheng@thoughtworks.com>
2018-07-19 22:09:59 +0800

    fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -65,7 +65,7 @@ task :deploy do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
     invoke :'git:clone'
-    invoke :'sidekiq:quiet'
+    # invoke :'sidekiq:quiet'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
