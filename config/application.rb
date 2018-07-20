e4d0017a157e4f1eb8f858889c601b5ea6d80029
ljcheng <ljcheng@thoughtworks.com>
2018-07-20 16:21:36 +0800

    eager load libs

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module MyApiApp
     config.middleware.use ActionDispatch::Session::CookieStore
 
     config.active_job.queue_adapter = :sidekiq
-    config.autoload_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
     config.redis = config_for(:redis)
   end
 end
