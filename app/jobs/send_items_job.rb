dc0a2f51d8dede2f77642b9d1f0b333172ba1341
ljcheng <ljcheng@thoughtworks.com>
2018-07-31 15:56:29 +0800

    update send items job

--- app/jobs/send_items_job.rb
+++ app/jobs/send_items_job.rb
@@ -3,7 +3,6 @@ class SendItemsJob < ApplicationJob
 
   def perform(account_id, target_id)
     account = Account.find(account_id)
-    account.asf('2fano')
     account.reload_inventory
     account.send_items(Account.find(target_id))
     account.asf('2faok')
