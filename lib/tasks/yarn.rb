8ff26620369b0942fcb57cab89b087903cc6ceb9
ljcheng <ljcheng@thoughtworks.com>
2018-08-02 13:17:41 +0800

    fix yarn task

--- lib/tasks/yarn.rb
+++ lib/tasks/yarn.rb
@@ -3,7 +3,7 @@ set :yarn_options, '--production'
 
 namespace :yarn do
   desc 'Install node modules using Yarn.'
-  task install: :environment do
+  task install: :remote_environment do
     comment 'Installing node modules using Yarn'
     in_path(fetch(:current_path)) do
       command %[#{fetch(:yarn_bin)} install --modules-folder #{fetch(:shared_path)}/node_modules #{fetch(:yarn_options)}]
