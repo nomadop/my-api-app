50734ee4946ba1e16312fc0ab14f0d71ba113c3c
ljcheng <ljcheng@thoughtworks.com>
2018-08-02 09:56:47 +0800

    update import action of buy order controller

--- app/controllers/buy_orders_controller.rb
+++ app/controllers/buy_orders_controller.rb
@@ -1,6 +1,7 @@
 class BuyOrdersController < ApplicationController
   def import
     buy_orders = JSON.parse(request.raw_post)
+    BuyOrder.truncate
     count = buy_orders.blank? ? 0 : BuyOrder.import(buy_orders, on_duplicate_key_ignore: {
       conflict_target: :buy_orderid,
     }).ids.count
