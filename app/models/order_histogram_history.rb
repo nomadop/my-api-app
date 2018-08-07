45fd079da2da47bfe3b3c3f32732aad81a73cf6b
ljcheng <ljcheng@thoughtworks.com>
2018-08-07 15:49:15 +0800

    add scopes to order histogram history

--- app/models/order_histogram_history.rb
+++ app/models/order_histogram_history.rb
@@ -1,2 +1,6 @@
 class OrderHistogramHistory < ApplicationRecord
+  scope :since, ->(time) { where('created_at > ?', time) }
+  scope :with_in, ->(duration) { since(duration.ago) }
+  scope :with_timestamp, -> { where.not(created_at: nil) }
+  default_scope { order(:created_at) }
 end
