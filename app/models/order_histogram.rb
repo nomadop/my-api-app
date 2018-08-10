cf790943a0e89e84901306d5f0df4137cc98c3c3
ljcheng <ljcheng@thoughtworks.com>
2018-08-10 12:01:13 +0800

    reduce min schedule interval of order histogram

--- app/models/order_histogram.rb
+++ app/models/order_histogram.rb
@@ -1,6 +1,6 @@
 class OrderHistogram < ApplicationRecord
   MAX_SCHEDULE_INTERVAL = 8.day.to_i
-  MIN_SCHEDULE_INTERVAL = 1.5.hour.to_i
+  MIN_SCHEDULE_INTERVAL = 45.minute.to_i
 
   belongs_to :market_asset, primary_key: :item_nameid, foreign_key: :item_nameid
   has_many :my_listings, through: :market_asset
